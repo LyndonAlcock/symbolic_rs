13966bd11a29ec1dbd0b748ecebbf4593b314292
LyndonAlcock <86410002+LyndonAlcock@users.noreply.github.com>
2022-12-05 08:27:42 +0000

    declarative macros are not working, i will use proc macros

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,68 @@
-mod symbolic_macros;
+extern crate proc_macro;
+use proc_macro::TokenStream;
+pub enum Symbolic<'a>{
+    Identifier(&'a str),
+    Function(&'a str),
+    Separator(char),
+    Rational(isize, usize),
+    Operator(char)
+}
+
+pub enum Node<'a>{
+    Symbolic(Symbolic<'a>),
+    Expression{
+        op: Symbolic<'a>,
+        buf: Vec<&'a Self>
+    }
+}
+
+
+macro_rules! symbolic{
+    (fn $($expression:tt)*) => {
+        symbolic!(@munch ( $($expression)* ) -> [])
+    };
+    ( @munch ($num:literal $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Rational($num,1), ])
+    };
+    ( @munch ($param:ident $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Identifier(stringify!($param)), ])
+    };
+    ( @munch ($func:ident ($($expression:tt)*) $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ 
+            $($accum)* 
+            Function(stringify!(func)),
+            symbolic!(@munch ( $($expression)* ) -> []),
+            Seperator(")"),
+        ])
+    };
+    ( @munch (($($expression:tt)*) $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ 
+            $($accum)* 
+            Symbolic::Operator('('),
+            symbolic!(@munch ( $($expression)* ) -> []),
+            Symbolic::Separator(')'),
+        ])
+    };
+    ( @munch (+ $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Operator('+'), ])
+    };
+    ( @munch (- $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Operator('-'), ])
+    };
+    ( @munch (/ $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Operator('/'), ])
+    };
+    ( @munch (* $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Operator('*'), ])
+    };
+    ( @munch (^ $($rest:tt)*) -> [$($accum:tt)*] ) => {
+        symbolic!(@munch ( $($rest)* ) -> [ $($accum)* Symbolic::Operator('^'), ])
+    };
+    ( @munch () -> [$($accum:tt)*] ) => {
+        vec![ $($accum)* ]
+    };
+}
 
 fn main() {
-    //symbolic!{fn f(x, a, b) -> 2 * 1 * 4}
-    //format_args!()
+    let func = symbolic!{fn a * sin(a * x) + x^2};
 }
